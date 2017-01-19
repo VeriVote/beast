4dc0836ebffbfa5009d67efd67045e38012efd28
Niels <niels.amazon@gmail.com>
2017-01-19 01:01:53 +0100

    added checkstyle and done some coding in the datatype package
    implemented 2 datatype classes: InternalType und InternalTypeContainer

---
+++ beast/src/main/java/edu/pse/beast/datatypes/InternalType.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.datatypes;
+
+/**
+ * Represents the InternalType of the Variables
+ * @author Niels
+ */
+
+public enum InternalType {
+    VOTER, CANDIDATE, SEAT, APPROVAL, WEIGHTEDAPPROVAL, INTEGER
+}
--- beast/src/main/java/edu/pse/beast/datatypes/InternalTypeContainer.java
+++ beast/src/main/java/edu/pse/beast/datatypes/InternalTypeContainer.java
@@ -6,9 +6,53 @@
 package edu.pse.beast.datatypes;
 
 /**
- *
+ * 
  * @author Niels
  */
-public class InternalTypeContainer {
-    
+public class InternalTypeContainer 
+{
+    private final boolean isList; 
+    private final InternalType internalType;
+    private final InternalTypeContainer listedType;
+    /**
+     * Constructor for a listed TypeContainer
+     * @param internalType sets the InternalType
+     * @param listedType sets the Type of the listelements
+     */
+    InternalTypeContainer(InternalType internalType, InternalTypeContainer listedType) {
+        this.isList = true;
+        this.internalType = internalType;
+        this.listedType = listedType;
+    }
+    /**
+     * Constructor for a TypeContainer that is NOT a List
+     * @param internalType 
+     */
+    InternalTypeContainer(InternalType internalType) {
+        this.isList = false;
+        this.internalType = internalType;
+        this.listedType = null;
+    }
+    /**
+     * Returns if the TypeContainer is a list
+     * @return isList
+     */
+    public boolean isList() {
+        return isList;
+    }
+    /**
+     * getter for internalType
+     * @return returns internalType
+     */
+    public InternalType getInternalType() {
+        return internalType;
+    }
+    /**
+     * getter for listedType
+     * If isList is false it returns null
+     * @return returns listedType
+     */
+    public InternalTypeContainer getListedType() {
+        return listedType;
+    }
 }
