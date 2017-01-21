2e8e50ad6f5480007475b35648da7a2d984f34d9
Niels <niels.amazon@gmail.com>
2017-01-21 02:03:05 +0100

    added a few sketchy Datatypes
    work in progress. Still need tests, more code and comments

---
+++ beast/src/main/java/edu/pse/beast/datatypes/FormalPropertiesDescription.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.datatypes;
+
+import edu.pse.beast.toolbox.BooleanExpCodeToASTConverter;
+
+/**
+ *
+ * @author Niels
+ */
+public class FormalPropertiesDescription {
+
+    private String code;
+    private BooleanExpCodeToASTConverter converter;
+
+    public FormalPropertiesDescription(BooleanExpCodeToASTConverter convertern) {
+        this.converter = converter;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+}
