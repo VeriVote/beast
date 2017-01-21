e76ad91c4179686fa660fdb0776edeac42a01739
Holger "big biceps" Klein <holgerhugoklein@gmail.com>
2017-01-21 18:50:16 +0100

    added interfaces ElectionDescriptionSource and PostAndPrePropertiesDescriptionSource

---
+++ beast/src/main/java/edu/pse/beast/highlevel/PostAndPrePropertiesDescriptionSource.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.highlevel;
+
+import edu.pse.beast.datatypes.propertydescription.PostAndPrePropertiesDescription;
+
+/**
+ *
+ * @author Holger-Desktop
+ */
+public interface PostAndPrePropertiesDescriptionSource {
+    public PostAndPrePropertiesDescription getPostAndPrePropertiesDescription(); 
+}
