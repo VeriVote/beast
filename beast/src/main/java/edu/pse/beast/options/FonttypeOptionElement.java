dfdf89863e401f3076a5514970d27987ff7c3f1e
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-01-22 02:22:26 +0100

    git didn't take this at first

--- beast/src/main/java/edu/pse/beast/options/FonttypeOptionElement.java
+++ beast/src/main/java/edu/pse/beast/options/FonttypeOptionElement.java
@@ -1,5 +1,33 @@
 package edu.pse.beast.options;
 
-public class FonttypeOptionElement {
+import java.util.List;
 
+public class FontTypeOptionElement extends OptionElement {
+
+    private final String chosenType;
+
+    /**
+     * 
+     * @param id the id
+     * @param choosableOptions the choosable options
+     * @param chosenType the chosen type
+     * @param chosenType
+     */
+    public FontTypeOptionElement(String id, List<String> choosableOptions, String chosenType) {
+        super(id, choosableOptions);
+        this.chosenType = chosenType;
+    }
+
+    /**
+     * 
+     * @return the chosen type
+     */
+    public String getChosenType() {
+        return chosenType;
+    }
+
+    @Override
+    public void handleSelection(String selection) {
+        // TODO Auto-generated method stub
+    }
 }
