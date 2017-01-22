0685fb74ffa3e46b0dcb990846936f82218abc36
Niels <niels.amazon@gmail.com>
2017-01-22 23:31:58 +0100

    main class now starts some of the GUI
    main class now starts some of the GUI

--- beast/src/main/java/edu/pse/beast/highlevel/MainClass.java
+++ beast/src/main/java/edu/pse/beast/highlevel/MainClass.java
@@ -1,18 +1,41 @@
-
 package edu.pse.beast.highlevel;
+
+import edu.pse.beast.booleanexpeditor.BooleanExpEditorBuilder;
+import edu.pse.beast.celectiondescriptioneditor.CElectionDescriptionEditorBuilder;
+import edu.pse.beast.saverloader.SaverLoaderInterface;
+import edu.pse.beast.stringresource.StringLoaderInterface;
+import edu.pse.beast.toolbox.ObjectRefsForBuilder;
+import edu.pse.beast.options.OptionsInterface;
+import edu.pse.beast.propertylist.PropertyListBuilder;
+import toBeImplemented.LanguageOptions;
 
 /**
  * The MainClass creates an AbstractBeastFactory and with it a BEASTCommunicator
  * which starts all processes to run BEAST.
+ *
  * @author Jonas
  */
-
 public class MainClass {
+
     /**
      * Starts BEAST
-     * @param args 
+     *
+     * @param args
      */
     public static void main(String[] args) {
+        OptionsInterface opt = new OptionsInterface();
+        ObjectRefsForBuilder objRefsForBuilder = new ObjectRefsForBuilder(new toBeImplemented.OptionsInterface(),
+                new StringLoaderInterface("de"),
+                new toBeImplemented.LanguageOptions(), new SaverLoaderInterface());
+
+        CElectionDescriptionEditorBuilder cElectionEditorBuilder = new CElectionDescriptionEditorBuilder();
+        cElectionEditorBuilder.createCElectionDescriptionEditor(objRefsForBuilder);
         
+        PropertyListBuilder propertyBuilder = new PropertyListBuilder();
+        propertyBuilder.createPropertyList(objRefsForBuilder);
+        
+        BooleanExpEditorBuilder booleanBuilder = new BooleanExpEditorBuilder();
+        booleanBuilder.createBooleanExpEditorObject(objRefsForBuilder);
+            
     }
 }
