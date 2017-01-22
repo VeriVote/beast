31129c2e07099a6f1800fcaf968616048fbf5656
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-01-22 23:42:58 +0100

    added optionsinterface

--- beast/src/main/java/edu/pse/beast/options/OptionsInterface.java
+++ beast/src/main/java/edu/pse/beast/options/OptionsInterface.java
@@ -1,5 +1,25 @@
 package edu.pse.beast.options;
 
+import edu.pse.beast.booleanexpeditor.BooleanExpEditor;
+import edu.pse.beast.parametereditor.ParameterEditor;
+import toBeImplemented.CElectionEditor;
+import toBeImplemented.CheckerList;
+import toBeImplemented.LanguageOptions;
+
 public class OptionsInterface {
-    //TODO
+
+    public BooleanExpEditorOptions getBooleanExpEditorOptions(BooleanExpEditor editor,
+            BooleanExpCodeAreaOptions booleanExpCodeAreaOptions) {
+        return new BooleanExpEditorOptions("id", editor, booleanExpCodeAreaOptions);
+    }
+
+    public CElectionEditorOptions getCElectionEditorOptions(CElectionEditor editor,
+            CElectionCodeAreaOptions cElectionCodeAreaOptions) {
+        return new CElectionEditorOptions("id", editor, cElectionCodeAreaOptions);
+    }
+
+    public ParametereditorOptions getParameterEditorOptions(CheckerOptionElement checker, CheckerList availableChecker, ParameterEditor editor, LanguageOptions langOpts) {
+        return new ParametereditorOptions("id", checker, availableChecker, langOpts);
+    }
+
 }
