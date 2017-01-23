bec05b4279daad08c086c61fd2df5fdb2dd7d364
NikolaiLMS <ufekm@student.kit.edu>
2017-01-23 04:01:53 +0100

    fixed ParameterEditorWindow icon loading

--- beast/src/main/java/edu/pse/beast/parametereditor/ParameterEditorWindow.java
+++ beast/src/main/java/edu/pse/beast/parametereditor/ParameterEditorWindow.java
@@ -64,7 +64,7 @@ public class ParameterEditorWindow extends javax.swing.JFrame {
 
         jToolBar.setRollover(true);
 
-        newIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/new.png"))); // NOI18N
+        newIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/new.png"))); // NOI18N
         newIcon.setFocusable(false);
         newIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         newIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
@@ -75,31 +75,31 @@ public class ParameterEditorWindow extends javax.swing.JFrame {
         });
         jToolBar.add(newIcon);
 
-        saveIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/save.png"))); // NOI18N
+        saveIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/save.png"))); // NOI18N
         saveIcon.setFocusable(false);
         saveIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         saveIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jToolBar.add(saveIcon);
 
-        saveAsIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/save_as.png"))); // NOI18N
+        saveAsIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/save_as.png"))); // NOI18N
         saveAsIcon.setFocusable(false);
         saveAsIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         saveAsIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jToolBar.add(saveAsIcon);
 
-        openIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/load.png"))); // NOI18N
+        openIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/load.png"))); // NOI18N
         openIcon.setFocusable(false);
         openIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         openIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jToolBar.add(openIcon);
 
-        startIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/start.png"))); // NOI18N
+        startIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/start.png"))); // NOI18N
         startIcon.setFocusable(false);
         startIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         startIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
         jToolBar.add(startIcon);
 
-        stopIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/stop.png"))); // NOI18N
+        stopIcon.setIcon(new javax.swing.ImageIcon(getClass().getResource("/images/toolbar/stop.png"))); // NOI18N
         stopIcon.setFocusable(false);
         stopIcon.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         stopIcon.setVerticalTextPosition(javax.swing.SwingConstants.BOTTOM);
