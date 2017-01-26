78ef74b36beed6f81fb4f11c80e0c1ce0719b2c1
Justin Hecht <justin.hecht@web.de>
2017-01-27 00:44:24 +0100

    Changes in the GUI to enable dynamic content

--- beast/src/main/java/edu/pse/beast/propertylist/PropertyListBuilder.java
+++ beast/src/main/java/edu/pse/beast/propertylist/PropertyListBuilder.java
@@ -55,11 +55,23 @@ public class PropertyListBuilder {
 
         ArrayList<ActionIdAndListener> fileList = new ArrayList<>();
         
+        UserAction newly = createNewPropertyList();
+        UserAction undo = createUndoChangesPropertyList();
+        UserAction redo = createRedoChangesPropertyList();
+        UserAction save = createSavePropertyList();
+        UserAction saveAs = createSaveAsPropertyList();
         UserAction load = createLoadPropertyList();
 
+        fileList.add(createFromUserAction(newly));
+
+        fileList.add(createFromUserAction(save));
+        fileList.add(createFromUserAction(saveAs));
         fileList.add(createFromUserAction(load));
 
         ArrayList<ActionIdAndListener> editList = new ArrayList<>();
+        
+        editList.add(createFromUserAction(undo));
+        editList.add(createFromUserAction(redo));
 
 
         created.add(fileList);
