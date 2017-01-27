385aaa7587fdb9c317501e6024a8633171441a27
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-01-27 09:59:53 +0100

    more work on the propertychecer

--- beast/src/main/java/edu/pse/beast/propertychecker/CheckerFactory.java
+++ beast/src/main/java/edu/pse/beast/propertychecker/CheckerFactory.java
@@ -21,13 +21,14 @@ import edu.pse.beast.toolbox.FileSaver;
 
 public abstract class CheckerFactory implements Runnable {
 
-    private Checker currentlyRunning;
-    private Thread workingThread;
     private final FactoryController controller;
     private final ElectionDescriptionSource electionDescSrc;
     private final PostAndPrePropertiesDescriptionSource postAndPrepPropDescSrc;
     private final ParameterSource paramSrc;
     private final Result result;
+    private Checker currentlyRunning;
+    private Thread workingThread;
+    private boolean stopped = false;
 
     private CheckerFactory(FactoryController controller, ElectionDescriptionSource electionDescSrc,
             PostAndPrePropertiesDescriptionSource postAndPrepPropDescSrc, ParameterSource paramSrc, Result result) {
@@ -40,33 +41,46 @@ public abstract class CheckerFactory implements Runnable {
     }
 
     public void run() {
-        
-        
+
         ArrayList<String> code = new CBMCCodeGenerator(null, null).getCode();
-        
-        //Get the file reference
+
+        // Get the file reference
         Path path = Paths.get("/Beast/src/main/resources/tmp/temp.c");
-        
-        
+
         FileSaver.writeStringLinesToFile(text, path, fileName);
-        
-        
-        //Use try-with-resource to get auto-closeable writer instance
 
-        
+        // Use try-with-resource to get auto-closeable writer instance
+
         paramSrc.getParameter().getAmountVoters();
-        
+
         paramSrc.getParameter().getAmountCandidates();
-        
+
         paramSrc.getParameter().getAmountSeats();
-        
+
         String advanced = paramSrc.getParameter().getAdvanced();
-
-
+        String params = advance
+        
+        for (Iterator<Integer> voteIterator = paramSrc.getParameter().getAmountVoters().iterator(); voteIterator.hasNext();) {
+            int voters = (int) voteIterator.next();
+            for (Iterator<Integer> candidateIterator = paramSrc.getParameter().getAmountVoters().iterator(); candidateIterator.hasNext();) {
+                int candidates = (int) candidateIterator.next();
+                for (Iterator<Integer> seatsIterator = paramSrc.getParameter().getAmountVoters().iterator(); seatsIterator.hasNext();) {
+                    int seats = (int) seatsIterator.next();
+                    
+                }
+            }
         }
+        
+
     }
 
-    public abstract void stopChecking();
+    public void stopChecking() {
+        currentlyRunning.interruptChecking();
+    }
+    
+    public void notifyThatFinished() {
+        
+    }
 
     /**
      * 
@@ -74,4 +88,6 @@ public abstract class CheckerFactory implements Runnable {
      *         factory
      */
     public abstract List<Result> getFittingResult(int size);
+    
+    protected abstract void startProcess(File toCheck, String callParams, Result result);
 }
