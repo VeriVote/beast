2279e2655936feaf77624796fa6ca33d1071e46d
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-01-27 10:15:20 +0100

    resolved conflicts

--- beast/src/main/java/edu/pse/beast/propertychecker/CheckerFactory.java
+++ beast/src/main/java/edu/pse/beast/propertychecker/CheckerFactory.java
@@ -21,13 +21,14 @@ import edu.pse.beast.toolbox.FileSaver;
 
 public abstract class CheckerFactory implements Runnable {
 
-    private Checker currentlyRunning;
-    private Thread workingThread;
     private final FactoryController controller;
     private final ElectionDescriptionSource electionDescSrc;
     private final PostAndPrePropertiesDescriptionSource postAndPrepPropDescSrc;
     private final ParameterSource paramSrc;
     private final Result result;
+    private Checker currentlyRunning;
+    private Thread workingThread;
+    private boolean stopped = false;
 
     private CheckerFactory(FactoryController controller, ElectionDescriptionSource electionDescSrc,
             PostAndPrePropertiesDescriptionSource postAndPrepPropDescSrc, ParameterSource paramSrc, Result result) {
@@ -40,38 +41,51 @@ public abstract class CheckerFactory implements Runnable {
     }
 
     public void run() {
-        
-        
-        ArrayList<String> code = new CBMCCodeGenerator(null, null).getCode();
-        
-        //Get the file reference
-        Path path = Paths.get("/Beast/src/main/resources/tmp/temp.c");
-        
-        
-//        FileSaver.writeStringLinesToFile(text, path, fileName);
-        
-        
-        //Use try-with-resource to get auto-closeable writer instance
 
-        
+        ArrayList<String> code = new CBMCCodeGenerator(null, null).getCode();
+
+        // Get the file reference
+        Path path = Paths.get("/Beast/src/main/resources/tmp/temp.c");
+
+        // FileSaver.writeStringLinesToFile(text, path, fileName);
+
+        // Use try-with-resource to get auto-closeable writer instance
+
         paramSrc.getParameter().getAmountVoters();
-        
+
         paramSrc.getParameter().getAmountCandidates();
-        
+
         paramSrc.getParameter().getAmountSeats();
-        
+
         String advanced = paramSrc.getParameter().getAdvanced();
 
+        for (Iterator<Integer> voteIterator = paramSrc.getParameter().getAmountVoters().iterator(); voteIterator
+                .hasNext();) {
+            int voters = (int) voteIterator.next();
+            for (Iterator<Integer> candidateIterator = paramSrc.getParameter().getAmountVoters()
+                    .iterator(); candidateIterator.hasNext();) {
+                int candidates = (int) candidateIterator.next();
+                for (Iterator<Integer> seatsIterator = paramSrc.getParameter().getAmountVoters()
+                        .iterator(); seatsIterator.hasNext();) {
+                    int seats = (int) seatsIterator.next();
 
+                }
+            }
         }
+
     }
 
-//    public abstract void stopChecking();
-
+    public void stopChecking() {
+        currentlyRunning.interruptChecking();
+    }
+    
+    protected abstract void startProcess(File toCheck, String callParams, Result result);
+    
     /**
      * 
      * @return the result object that belongs to the Checker produced by this
      *         factory
      */
-//    public abstract List<Result> getFittingResult(int size);
+    public abstract List<Result> getFittingResult(int size);
+}
 
