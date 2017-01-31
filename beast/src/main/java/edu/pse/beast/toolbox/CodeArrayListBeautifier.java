9c74dcd368109967b27d0cb29719eba03116c225
Niels <niels.amazon@gmail.com>
2017-01-31 17:08:34 +0100

    added a new Class CodeArrayListBeautifier
    it handels that tabs in the generated code.
    This was crucial for being able to read the generated Code

---
+++ beast/src/main/java/edu/pse/beast/toolbox/CodeArrayListBeautifier.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.toolbox;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Niels
+ */
+public class CodeArrayListBeautifier {
+
+    private int numberOfTabs;
+    private final ArrayList<String> code;
+
+    public CodeArrayListBeautifier() {
+        super();
+        code = new ArrayList<>();
+        numberOfTabs = 0;
+    }
+
+    public void add(String e) {
+        String tabbed = "";
+        for (int i = 0; i < numberOfTabs; i++) {
+            tabbed += "\t";
+        }
+        code.add(tabbed + e);
+    }
+
+    public void addTab() {
+        numberOfTabs++;
+    }
+
+    public void deleteTab() {
+        if (numberOfTabs > 0) {
+            numberOfTabs--;
+        } else {
+            ErrorLogger.log("number of tabs is allready 0");
+        }
+    }
+
+    public ArrayList<String> getCodeArrayList() {
+        return code;
+    }
+}
---
+++ beast/src/test/java/edu/pse/beast/toolbox/CodeArrayListBeautifierTest.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.toolbox;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Niels
+ */
+public class CodeArrayListBeautifierTest {
+
+    public CodeArrayListBeautifierTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class CodeArrayListBeautifier.
+     */
+    @Test
+    public void testAdd() {
+        System.out.println("add");
+        String e = "test";
+        CodeArrayListBeautifier instance = new CodeArrayListBeautifier();
+        instance.add(e);
+        assertEquals("test", (instance.getCodeArrayList().get(0)));
+    }
+
+    /**
+     * Test of addTab method, of class CodeArrayListBeautifier.
+     */
+    @Test
+    public void testAddTab() {
+        System.out.println("addTab");
+        System.out.println("add");
+        String e = "test";
+        CodeArrayListBeautifier instance = new CodeArrayListBeautifier();
+        instance.addTab();
+        instance.add(e);
+        assertEquals("\ttest", (instance.getCodeArrayList().get(0)));
+    }
+
+    /**
+     * Test of deleteTab method, of class CodeArrayListBeautifier.
+     */
+    @Test
+    public void testDeleteTab() {
+        System.out.println("deleteTab");
+        String e = "test";
+        CodeArrayListBeautifier instance = new CodeArrayListBeautifier();
+        instance.addTab();
+        
+        instance.deleteTab();
+        instance.add(e);
+        assertEquals("test", (instance.getCodeArrayList().get(0)));
+    }
+
+    /**
+     * Test of getCodeArrayList method, of class CodeArrayListBeautifier.
+     */
+    @Test
+    public void testGetCodeArrayList() {
+        String e = "test";
+        CodeArrayListBeautifier instance = new CodeArrayListBeautifier();
+        instance.add(e);
+        assertEquals("test", (instance.getCodeArrayList().get(0)));
+    }
+
+}
