cded11f3a59a5d5ec8d5b72aac3688c176fec928
NikolaiLMS <ufekm@student.kit.edu>
2017-02-02 23:01:20 +0100

    implemented FormalPropertyDescription grammar test class

--- beast/src/test/java/edu/pse/beast/BooleanExpEditor/ANTLRTest.java
+++ beast/src/test/java/edu/pse/beast/BooleanExpEditor/ANTLRTest.java
@@ -2,13 +2,32 @@ package edu.pse.beast.BooleanExpEditor;
 
 //import edu.pse.beast.toolbox.antlr.booleanexp.ANTLRBooleanExpressionListener;
 
+import edu.pse.beast.toolbox.antlr.booleanexp.FormalPropertyDescriptionLexer;
+import edu.pse.beast.toolbox.antlr.booleanexp.FormalPropertyDescriptionParser;
+import org.antlr.v4.gui.TreeViewer;
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author NikolaiLMS
  */
 public class ANTLRTest {
-    public static void main(String[] args) {
-        //ANTLRBooleanExpressionListener booleanExpressionListener = new ANTLRBooleanExpressionListener();
-        //booleanExpressionListener.printProperty("FOR_ALL_VOTERS(v) : VOTES1(v) == VOTES2(v)");
-    }
 
+    static void showGuiTreeView(final String code) {
+        final org.antlr.v4.runtime.CharStream stream = new ANTLRInputStream(code);
+        final FormalPropertyDescriptionLexer lexer = new FormalPropertyDescriptionLexer(stream);
+        final CommonTokenStream tokens = new CommonTokenStream(lexer);
+        final FormalPropertyDescriptionParser parser = new FormalPropertyDescriptionParser(tokens);
+        final ParseTree tree = parser.booleanExpList();
+        final List<String> ruleNames = Arrays.asList(FormalPropertyDescriptionParser.ruleNames);
+        final TreeViewer view = new TreeViewer(ruleNames, tree);
+        view.open();
+    }
+    public static void main(String[] args) {
+        showGuiTreeView("FOR_ALL_VOTERS(i) : ((i!=u && i!=w) ==> VOTES1(i) == VOTES2(i));");
+    }
 }
