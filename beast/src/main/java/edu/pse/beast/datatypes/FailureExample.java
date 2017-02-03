d5f0b2e6d15be36cf9504c001cbf37e33eab0385
Justin Hecht <justin.hecht@web.de>
2017-02-03 01:19:22 +0100

    first sketch of FailureExample

---
+++ beast/src/main/java/edu/pse/beast/datatypes/FailureExample.java
@@ -0,0 +1,26 @@
+package edu.pse.beast.datatypes;
+
+import edu.pse.beast.datatypes.internal.InternalTypeContainer;
+
+/**
+ * @author Justin
+ *
+ */
+public class FailureExample {
+	
+	private InternalTypeContainer internalType;
+	
+	private int[] votes1;
+	private int[] votes2;
+	private int[][] votelist1;
+	private int[][] votelist2;
+	
+	private int elect1;
+	private int elect2;
+	private int[] seats1;
+	private int[] seats2;
+	
+	private int numOfCandidates;
+	
+
+}
