68b45ef6645ffd48a62e170ed4504c6c8fcfde17
Justin Hecht <justin.hecht@web.de>
2017-02-03 16:02:01 +0100

    FailureExample class finalized

--- beast/src/main/java/edu/pse/beast/datatypes/FailureExample.java
+++ beast/src/main/java/edu/pse/beast/datatypes/FailureExample.java
@@ -26,6 +26,17 @@ public class FailureExample {
 	
 	private final int numOfVoters;
 
+	/**
+	 * Creates the FailureExample from the returned data of CBMC. If specific data is not given, initialize with null reference.
+	 * @param internalType The type of the election description
+	 * @param votes A list of votings for a single candidate given as an integer array. Null if another voting procedure was used.
+	 * @param voteList A list of votings for multiple candidates (maybe as a list or range voting). Null if only one candidate could be chosen.
+	 * @param elect A list of elected candidates from different votings. Null if more than one candidate is elected.
+	 * @param seats A list of elected seasts from different votings. Null if only one candidate is elected.
+	 * @param numOfCandidates The number of candidates in an election.
+	 * @param numOfSeats The number of seats that are awarded in an election.
+	 * @param numOfVoters The number of voters that voted for the candidates.
+	 */
 	public FailureExample(InternalTypeContainer internalType, List<int[]> votes, List<int[][]> voteList,
 			List<Integer> elect, List<int[]> seats, int numOfCandidates, int numOfSeats, int numOfVoters) {
 		
@@ -39,6 +50,47 @@ public class FailureExample {
 		this.numOfVoters = numOfVoters;
 
 	}
+
+	public InternalTypeContainer getInternalType() {
+		return internalType;
+	}
+
+	public List<int[]> getVotes() {
+		return votes;
+	}
+
+	public List<int[][]> getVoteList() {
+		return voteList;
+	}
+
+	public List<Integer> getElect() {
+		return elect;
+	}
+
+	public List<int[]> getSeats() {
+		return seats;
+	}
+
+	public int getNumOfCandidates() {
+		return numOfCandidates;
+	}
+
+	public int getNumOfSeats() {
+		return numOfSeats;
+	}
+
+	public int getNumOfVoters() {
+		return numOfVoters;
+	}
 	
+	public boolean isChooseOneCandidate() {
+		if (getVotes() != null) return true;
+		else return false;
+	}
+	
+	public boolean isOneSeatOnly() {
+		if (getElect() != null) return true;
+		return false;
+	}
 
 }
