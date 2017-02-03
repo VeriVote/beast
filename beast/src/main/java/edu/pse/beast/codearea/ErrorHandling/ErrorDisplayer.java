fdfe3e792035eb7ebad3f2bf3aa218d5b0d73d88
Holger "big biceps" Klein <holgerhugoklein@gmail.com>
2017-02-03 08:04:38 +0100

    a popupmenu now appears and shows the error message

--- beast/src/main/java/edu/pse/beast/codearea/ErrorHandling/ErrorDisplayer.java
+++ beast/src/main/java/edu/pse/beast/codearea/ErrorHandling/ErrorDisplayer.java
@@ -28,24 +28,30 @@ import javax.swing.text.View;
  *
  * @author Holger-Desktop
  */
-public abstract class ErrorDisplayer implements CaretListener, DisplaysStringsToUser {
+public abstract class ErrorDisplayer implements DisplaysStringsToUser, MouseMotionListener {
     protected JTextPane pane;
     private SquigglePainter painter;
     private LineHandler lineHandler;
-    private HashMap< Tuple<Integer,Integer>, CodeError> absPosToError;
+    private ArrayList<Tuple<Integer,Integer>> absPosToMsg;
+    private ArrayList<String> msges;
     protected StringResourceLoader currentStringResLoader;
-    private ArrayList<Object> highLights = new ArrayList<>();
+    private ArrayList<Object> highLights = new ArrayList<>();    
+    private ErrorPopupMenu errorPopupMenu = new ErrorPopupMenu();
+    
     public ErrorDisplayer(JTextPane pane, StringResourceLoader currentStringResLoader) {
-        absPosToError = new HashMap<>();
+        absPosToMsg = new ArrayList<>();
+        msges = new ArrayList<>();
         this.pane = pane;
-        pane.addCaretListener(this);
+        pane.addMouseMotionListener(this);
         this.painter = new SquigglePainter(Color.red);        
         this.lineHandler = new LineHandler(pane);
         this.currentStringResLoader = currentStringResLoader;
     }
     
     public void showErrors(ArrayList<CodeError> errors) {
-        absPosToError = new HashMap<>();
+        absPosToMsg = new ArrayList<>();
+        msges = new ArrayList<>();
+        
         for(Object o : highLights) pane.getHighlighter().removeHighlight(o);
     }
     
@@ -54,7 +60,8 @@ public abstract class ErrorDisplayer implements CaretListener, DisplaysStringsTo
         int startpos = er.getStartPos();
         int endpos = er.getEndPos();
         if(startpos == endpos) endpos++;
-        absPosToError.put(new Tuple<>(startpos, endpos), er);
+        absPosToMsg.add(new Tuple<>(startpos, endpos));
+        msges.add(msg);
         try {
             highLights.add(pane.getHighlighter().addHighlight(startpos, endpos + 1, painter));
         } catch (BadLocationException ex) {
@@ -72,12 +79,29 @@ public abstract class ErrorDisplayer implements CaretListener, DisplaysStringsTo
         template = template.replace("ERRNO", String.valueOf(er.getErrorNumber()));
         return template;
     }
-
     @Override
-    public void caretUpdate(CaretEvent ce) {
-        CodeError err = absPosToError.get(ce.getDot());
-        if(err == null) return;
-        System.err.println(err.getId());
+    public void mouseMoved(MouseEvent e) {
+        Point pt = new Point(e.getX(), e.getY());
+        int pos = pane.viewToModel(pt);
+        System.out.println("m abs " + pt.x + "," + pt.y + " m_pos: " + pos);
+        if(Math.abs(errorPopupMenu.getLocation().x - pt.getX()) < 10 && Math.abs(errorPopupMenu.getLocation().x - pt.getX()) < 10 && errorPopupMenu.isVisible()) return;
+        for(int i = 0; i < absPosToMsg.size(); ++i) {
+            if(absPosToMsg.get(i).x <= pos && absPosToMsg.get(i).y >= pos) {
+                errorPopupMenu.getErrorItem().setText(msges.get(i));
+                errorPopupMenu.show(pane, e.getX(), e.getY());
+                System.out.println("showing err window");
+                return;
+            }
+        }
+        if(errorPopupMenu.isVisible()) {            
+            errorPopupMenu.setVisible(false);
+            System.out.println("hiding err window");
+        }
+    }
+    
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        
     }
 
     
---
+++ beast/src/main/java/edu/pse/beast/codearea/ErrorHandling/ErrorPopupMenu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.codearea.ErrorHandling;
+
+import java.awt.Color;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+/**
+ *
+ * @author Holger-Desktop
+ */
+public class ErrorPopupMenu extends JPopupMenu {
+    JMenuItem errorItem;
+    public ErrorPopupMenu(){
+        errorItem = new JMenuItem("");
+        errorItem.setBackground(Color.white);
+        add(errorItem);
+    }
+
+    public JMenuItem getErrorItem() {
+        return errorItem;
+    }    
+}
