65e6c88694180ffe9d7ab8aad8ee1ec271ab3cba
Holger "big biceps" Klein <holgerhugoklein@gmail.com>
2017-02-11 18:20:02 +0100

    added RepaintThread

---
+++ beast/src/main/java/edu/pse/beast/toolbox/RepaintThread.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.toolbox;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Holger-Desktop
+ */
+public class RepaintThread implements Runnable {
+    private JFrame frame;
+    public RepaintThread(JFrame frame) {
+        this.frame = frame;
+    }
+
+    @Override
+    public void run() {
+        while(true) {
+            try {
+                Thread.sleep(1000/60);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(RepaintThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            frame.repaint();
+        }
+    }
+}
