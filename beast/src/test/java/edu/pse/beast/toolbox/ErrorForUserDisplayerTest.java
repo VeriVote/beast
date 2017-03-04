a038ea6ad9d552aa5891dc4f45d7b8c4e22d0e8e
Niels <niels.amazon@gmail.com>
2017-03-04 14:13:13 +0100

    added the errorPopupWindow
    added a simple Test for the ErrorForUserDisplayer, which is sequential

---
+++ beast/src/test/java/edu/pse/beast/toolbox/ErrorForUserDisplayerTest.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pse.beast.toolbox;
+
+/**
+ *
+ * @author Niels
+ */
+public class ErrorForUserDisplayerTest {
+    
+    /**
+     * a small main class Test for the ErrorForUserDisplayer
+     * @param args 
+     */
+    public static void main(String args[]) {
+        ErrorForUserDisplayer.displayError("hi");
+        ErrorForUserDisplayer.displayError("hi again");
+        ErrorForUserDisplayer.displayError("hi again2");
+    }
+}
