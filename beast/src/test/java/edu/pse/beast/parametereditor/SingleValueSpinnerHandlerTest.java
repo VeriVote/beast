ae7dbbc35d057406666cdb1cbb371f00ea79c797
Jonas Wohnig <jonas.wohnig@gmx.net>
2017-03-06 11:07:23 +0100

    added tests for SpinnerHandlers

---
+++ beast/src/test/java/edu/pse/beast/parametereditor/SingleValueSpinnerHandlerTest.java
@@ -0,0 +1,29 @@
+package edu.pse.beast.parametereditor;
+
+import javax.swing.JSpinner;
+import javax.swing.event.ChangeEvent;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Jonas
+ */
+public class SingleValueSpinnerHandlerTest {
+    
+    /**
+     * Test of setValue and getValue methods, of class SingleValueSpinnerHandler.
+     */
+    @Test
+    public void testSetGetValue() {
+        System.out.println("set/getValue");
+        SingleValueSpinnerHandler instance = new SingleValueSpinnerHandler(new JSpinner());
+        Integer expResult = 42;
+        instance.setValue(42);
+        instance.setValue(-1);
+        instance.setValue(10001);
+        Integer result = instance.getValue();
+        assertEquals(expResult, result);
+    }
+
+}
