5da299bf2b1c31b76f99d6395fabaf310274e383
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-03-07 08:45:42 +0100

    refactored the checkerfactoryfactory a bit and implemented tests for it

--- beast/src/test/java/edu/pse/beast/propertychecker/CheckerFactoryFactoryTest.java
+++ beast/src/test/java/edu/pse/beast/propertychecker/CheckerFactoryFactoryTest.java
@@ -1,16 +1,57 @@
 package edu.pse.beast.propertychecker;
 
-import org.junit.After;
+
+import static org.junit.Assert.*;
+
+import java.util.List;
+
 import org.junit.Before;
+import org.junit.Test;
 
 public class CheckerFactoryFactoryTest {
 
     @Before
     public void setUp() {
+        
     }
 
-    @After
-    public void tearDown() {
+    @Test
+    public void createSuccess() {
+        PropertyChecker checker = CheckerFactoryFactory.createPropertyChecker("cbmc");
+        assertNotNull(checker);
     }
 
+    @Test
+    public void createFailure() {
+        assertNull(CheckerFactoryFactory.createPropertyChecker("nicht_cbmc"));
+    }
+    
+    @Test
+    public void resultSuccess() {
+        List<Result> result = CheckerFactoryFactory.getMatchingResult("cbmc", 1);
+        assertNotNull(result);
+        assertTrue(result.get(0) instanceof CBMCResult);
+    }
+    
+    @Test
+    public void resultFailure() {
+        PropertyChecker checker = (PropertyChecker) CheckerFactoryFactory.getMatchingResult("nicht_cbmc", 1);
+        assertNull(checker);
+    }
+    
+    @Test
+    public void createCheckerfactorySuccess() {
+        CheckerFactory factory = CheckerFactoryFactory.getCheckerFactory("cbmc", null, null, null, null, null);
+        
+        assertNotNull(factory);
+        
+        assertTrue(factory instanceof CBMCProcessFactory);
+    }
+    
+    @Test
+    public void createCheckerfactoryFailure() {
+        CheckerFactory factory = CheckerFactoryFactory.getCheckerFactory("nicht_cbmc", null, null, null, null, null);
+        
+        assertNull(factory);
+    }
 }
