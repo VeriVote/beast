cd9d2adadcdf0805af6748b45c6255e4591a8066
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2017-03-14 09:11:54 +0100

    fixed the finding of the helperfile.html

--- beast/src/main/java/edu/pse/beast/booleanexpeditor/UserActions/ShowHelpUserAction.java
+++ beast/src/main/java/edu/pse/beast/booleanexpeditor/UserActions/ShowHelpUserAction.java
@@ -2,6 +2,7 @@ package edu.pse.beast.booleanexpeditor.UserActions;
 
 import edu.pse.beast.highlevel.DisplaysStringsToUser;
 import edu.pse.beast.stringresource.StringLoaderInterface;
+import edu.pse.beast.toolbox.SuperFolderFinder;
 import edu.pse.beast.toolbox.UserAction;
 
 import java.awt.*;
@@ -23,7 +24,7 @@ public class ShowHelpUserAction extends UserAction implements DisplaysStringsToU
 
     @Override
     public void perform() {
-        File f = new File("./" + pathToHelpFile);
+        File f = new File(SuperFolderFinder.getSuperFolder() + "./" + pathToHelpFile);
         try {
             Desktop.getDesktop().browse(f.toURI());
         } catch (IOException e) {
