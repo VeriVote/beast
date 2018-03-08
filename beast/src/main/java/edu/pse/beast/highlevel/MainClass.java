b049f6da1befc400a82743cabfda3a1d45475fbd
Lukas Stapelbroek <lukas-stapelbroek@gmx.de>
2018-03-08 17:20:54 +0100

    quick push

--- beast/src/main/java/edu/pse/beast/highlevel/MainClass.java
+++ beast/src/main/java/edu/pse/beast/highlevel/MainClass.java
@@ -1,24 +1,59 @@
 package edu.pse.beast.highlevel;
 
+import java.net.URL;
+import java.util.ListResourceBundle;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
 /**
  * The MainClass creates an CentralObjectProvider which creates all other parts
  * of the program and with it a BEASTCommunicator which coordinates them.
  *
  * @author Jonas
  */
-public class MainClass {
+public class MainClass extends Application {
+	
+	/**
+	 * Starts BEAST by creating a BEASTCommunicator and corresponding
+	 * CentralObjectProvider. If you want to replace one or more implementation of
+	 * high level interfaces you have to create a new implementation of
+	 * CentralObjectProvider and replace PSECentralObjectProvider with it here.
+	 *
+	 * @param args
+	 *            not used
+	 */
+	public static void main(String[] args) {
+		BEASTCommunicator communicator = new BEASTCommunicator();
+		PSECentralObjectProvider centralObjectProvider = new PSECentralObjectProvider(communicator);
+		communicator.setCentralObjectProvider(centralObjectProvider);
+		launch(args);
+	}
 
-    /**
-     * Starts BEAST by creating a BEASTCommunicator and corresponding CentralObjectProvider.
-     * If you want to replace one or more implementation of high level interfaces you have to
-     * create a new implementation of CentralObjectProvider and replace PSECentralObjectProvider
-     * with it here.
-     *
-     * @param args not used
-     */
-    public static void main(String[] args) {
-        BEASTCommunicator communicator = new BEASTCommunicator();
-        PSECentralObjectProvider centralObjectProvider = new PSECentralObjectProvider(communicator);
-        communicator.setCentralObjectProvider(centralObjectProvider);
-    }
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+		Locale local = Locale.getDefault();
+		
+		try {
+			
+			Parent root = FXMLLoader.load(getClass().getResource("javafx/BEAST.fxml"), ResourceBundle.getBundle("edu.pse.beast.highlevel.javafx.bundles.LangBundle", local));
+			
+			
+			Scene scene = new Scene(root, 800, 600);
+
+			stage.setTitle("BEAST");
+			stage.setScene(scene);
+			stage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
---
+++ beast/src/main/java/edu/pse/beast/toolbox/Internationalization.java
@@ -0,0 +1,154 @@
+package edu.pse.beast.toolbox;
+
+/**
+ * Copyright (c) 2016 sothawo
+ *
+ * http://www.sothawo.com
+ */
+
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.StringBinding;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tooltip;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.concurrent.Callable;
+
+/**
+ * I18N utility class..
+ *
+ * @author P.J. Meisch (pj.meisch@sothawo.com).
+ * 
+ * taken from: https://www.sothawo.com/2016/09/how-to-implement-a-javafx-ui-where-the-language-can-be-changed-dynamically/
+ */
+public final class Internationalization {
+
+    /** the current selected Locale. */
+    private static final ObjectProperty<Locale> locale;
+
+    static {
+        locale = new SimpleObjectProperty<>(getDefaultLocale());
+        locale.addListener((observable, oldValue, newValue) -> Locale.setDefault(newValue));
+    }
+
+    /**
+     * get the supported Locales.
+     *
+     * @return List of Locale objects.
+     */
+    public static List<Locale> getSupportedLocales() {
+        return new ArrayList<>(Arrays.asList(Locale.ENGLISH, Locale.GERMAN));
+    }
+
+    /**
+     * get the default locale. This is the systems default if contained in the supported locales, english otherwise.
+     *
+     * @return
+     */
+    public static Locale getDefaultLocale() {
+        Locale sysDefault = Locale.getDefault();
+        return getSupportedLocales().contains(sysDefault) ? sysDefault : Locale.ENGLISH;
+    }
+
+    public static Locale getLocale() {
+        return locale.get();
+    }
+
+    public static void setLocale(Locale locale) {
+        localeProperty().set(locale);
+        Locale.setDefault(locale);
+    }
+
+    public static ObjectProperty<Locale> localeProperty() {
+        return locale;
+    }
+
+    /**
+     * gets the string with the given key from the resource bundle for the current locale and uses it as first argument
+     * to MessageFormat.format, passing in the optional args and returning the result.
+     *
+     * @param key
+     *         message key
+     * @param args
+     *         optional arguments for the message
+     * @return localized formatted string
+     */
+    public static String get(final String key, final Object... args) {
+        ResourceBundle bundle = ResourceBundle.getBundle("messages", getLocale());
+        return MessageFormat.format(bundle.getString(key), args);
+    }
+
+    /**
+     * creates a String binding to a localized String for the given message bundle key
+     *
+     * @param key
+     *         key
+     * @return String binding
+     */
+    public static StringBinding createStringBinding(final String key, Object... args) {
+        return Bindings.createStringBinding(() -> get(key, args), locale);
+    }
+
+    /**
+     * creates a String Binding to a localized String that is computed by calling the given func
+     *
+     * @param func
+     *         function called on every change
+     * @return StringBinding
+     */
+    public static StringBinding createStringBinding(Callable<String> func) {
+        return Bindings.createStringBinding(func, locale);
+    }
+
+    /**
+     * creates a bound Label whose value is computed on language change.
+     *
+     * @param func
+     *         the function to compute the value
+     * @return Label
+     */
+    public static Label labelForValue(Callable<String> func) {
+        Label label = new Label();
+        label.textProperty().bind(createStringBinding(func));
+        return label;
+    }
+
+    /**
+     * creates a bound Button for the given resourcebundle key
+     *
+     * @param key
+     *         ResourceBundle key
+     * @param args
+     *         optional arguments for the message
+     * @return Button
+     */
+    public static Button buttonForKey(final String key, final Object... args) {
+        Button button = new Button();
+        button.textProperty().bind(createStringBinding(key, args));
+        return button;
+    }
+
+    /**
+     * creates a bound Tooltip for the given resourcebundle key
+     *
+     * @param key
+     *         ResourceBundle key
+     * @param args
+     *         optional arguments for the message
+     * @return Label
+     */
+    public static Tooltip tooltipForKey(final String key, final Object... args) {
+        Tooltip tooltip = new Tooltip();
+        tooltip.textProperty().bind(createStringBinding(key, args));
+        return tooltip;
+    }
+
+}
