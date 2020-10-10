627899bdf06a433af4d0dcb24731a676455e584c
Michael Kirsten <kirsten@kit.edu>
2020-10-10 02:06:26 +0200

    Minor fix within last cleanup

--- beast/src/main/java/edu/pse/beast/propertychecker/WindowsProcess.java
+++ beast/src/main/java/edu/pse/beast/propertychecker/WindowsProcess.java
@@ -100,7 +100,7 @@ public final class WindowsProcess extends CBMCProcess {
             flat += (i != 0 && i != list.size() ? BLANK : "") + l;
             i++;
         }
-        return "";
+        return flat;
     }
 
     /**
