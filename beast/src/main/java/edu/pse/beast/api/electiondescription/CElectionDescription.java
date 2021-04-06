b7bb141b37fa2bb3da875217ef331594d8924ee0
Holger Klein <holgerhugoklein@gmail.com>
2021-04-06 10:20:08 +0200

    updated fxml for loop bounds

--- beast/src/main/java/edu/pse/beast/api/electiondescription/CElectionDescription.java
+++ beast/src/main/java/edu/pse/beast/api/electiondescription/CElectionDescription.java
@@ -1,11 +1,16 @@
 package edu.pse.beast.api.electiondescription;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+import edu.pse.beast.api.codegen.loopbounds.LoopBound;
 
 public class CElectionDescription {
 	private List<VotingSigFunction> votingSigFunctions = new ArrayList<>();
 	private List<SimpleTypeFunction> simpleTypeFunctions = new ArrayList<>();
+	private Map<String, List<LoopBound>> loopBounds = new HashMap();
 	private String name;
 
 	private VotingSigFunction votingFunction;
@@ -13,10 +18,8 @@ public class CElectionDescription {
 	private VotingInputTypes inputType;
 	private VotingOutputTypes outputType;
 
-	public CElectionDescription(
-			VotingInputTypes inputType, 
-			VotingOutputTypes outputType,
-			String name) {
+	public CElectionDescription(VotingInputTypes inputType,
+			VotingOutputTypes outputType, String name) {
 		this.inputType = inputType;
 		this.outputType = outputType;
 		this.name = name;
@@ -25,28 +28,38 @@ public class CElectionDescription {
 
 	public VotingSigFunction getVotingFunction() {
 		return votingFunction;
-	}	
-	
+	}
+
 	public void setVotingFunction(VotingSigFunction votingFunction) {
 		this.votingFunction = votingFunction;
 	}
-	
+
 	public VotingSigFunction createNewVotingSigFunctionAndAdd(String name) {
-		VotingSigFunction created = new VotingSigFunction(name, inputType, outputType);
+		VotingSigFunction created = new VotingSigFunction(name, inputType,
+				outputType);
 		votingSigFunctions.add(created);
 		return created;
 	}
-	
+
 	public VotingInputTypes getInputType() {
 		return inputType;
 	}
-	
+
 	public VotingOutputTypes getOutputType() {
 		return outputType;
 	}
-	
+
 	public List<VotingSigFunction> getVotingSigFunctions() {
 		return votingSigFunctions;
 	}
-	
+
+	public List<LoopBound> getLoopBoundsForFunction(String name) {
+		return loopBounds.get(name);
+	}
+
+	public void addLoopBoundForFunction(String functionName, String loopIndex,
+			String bound) {
+		
+	}
+
 }
