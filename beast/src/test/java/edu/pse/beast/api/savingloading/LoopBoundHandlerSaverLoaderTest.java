f5c38e02a20058de70e1ae81cf706a550611e5f3
Holger Klein <holgerhugoklein@gmail.com>
2021-05-09 13:01:54 +0200

    finished test for LoopboundSaverLoader

--- beast/src/test/java/edu/pse/beast/api/savingloading/LoopBoundHandlerSaverLoaderTest.java
+++ beast/src/test/java/edu/pse/beast/api/savingloading/LoopBoundHandlerSaverLoaderTest.java
@@ -1,8 +1,14 @@
 package edu.pse.beast.api.savingloading;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.json.JSONObject;
 import org.junit.Test;
 
+import edu.pse.beast.api.codegen.loopbounds.LoopBound;
 import edu.pse.beast.api.codegen.loopbounds.LoopBoundHandler;
 import edu.pse.beast.api.codegen.loopbounds.LoopBoundType;
 import edu.pse.beast.api.savingloading.testruns.LoopBoundHandlerSaverLoaderHelper;
@@ -28,6 +34,23 @@ public class LoopBoundHandlerSaverLoaderTest {
 
 		LoopBoundHandler loadedLoopBoundHandler = LoopBoundHandlerSaverLoaderHelper
 				.loopBoundHandlerFromJSON(json);
+		
+		for(String name : loadedLoopBoundHandler.getFuncNameToLoopbounds().keySet()) {
+			assertTrue(loopBoundHandler.getFuncNameToLoopbounds().containsKey(name));
+		}
+		
+		for(String name : loadedLoopBoundHandler.getFuncNameToLoopbounds().keySet()) {
+			List<LoopBound> l1 = loadedLoopBoundHandler.getFuncNameToLoopbounds().get(name);
+			List<LoopBound> l2 = loopBoundHandler.getFuncNameToLoopbounds().get(name);
+			
+			assertEquals(l1.size(), l2.size());
+			
+			for(int i = 0;i < l1.size(); ++i) {
+				assertEquals(l1.get(i).getFunctionName(), l2.get(i).getFunctionName());
+				assertEquals(l1.get(i).getIndex(), l2.get(i).getIndex());
+				assertEquals(l1.get(i).getLoopBoundType(), l2.get(i).getLoopBoundType());
+			}
+		}
 		
 	}
 }
