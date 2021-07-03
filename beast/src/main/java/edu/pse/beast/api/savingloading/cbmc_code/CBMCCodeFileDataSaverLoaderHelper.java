cd3311c14b9552382721c8447546e85cdc70692a
Holger Klein <holgerhugoklein@gmail.com>
2021-07-03 20:38:50 +0200

    using relative file paths for generated c files

--- beast/src/main/java/edu/pse/beast/api/savingloading/cbmc_code/CBMCCodeFileDataSaverLoaderHelper.java
+++ beast/src/main/java/edu/pse/beast/api/savingloading/cbmc_code/CBMCCodeFileDataSaverLoaderHelper.java
@@ -6,18 +6,20 @@ import org.json.JSONObject;
 
 import edu.pse.beast.api.codegen.cbmc.info.CBMCGeneratedCodeInfo;
 import edu.pse.beast.api.testrunner.CBMCCodeFileData;
+import edu.pse.beast.gui.paths.PathHandler;
 
 public class CBMCCodeFileDataSaverLoaderHelper {
 
 	private static final String CBMC_GENERATED_CODE_INFO_KEY = "cbmc_generated_code_info";
-	private static final String CBMC_TEST_RUN_CBMC_FILE_PATH_KEY = "cbmc_test_run_cbmc_file_path";
+	private static final String CBMC_TEST_RUN_CBMC_REL_FILE_PATH_KEY = "cbmc_test_run_cbmc_file_path";
 
 	public static JSONObject cbmcCodeFileToJSON(
-			CBMCCodeFileData cbmcCodeFileData) {
+			CBMCCodeFileData cbmcCodeFileData, PathHandler pathHandler) {
 		JSONObject json = new JSONObject();
 
-		json.put(CBMC_TEST_RUN_CBMC_FILE_PATH_KEY,
-				cbmcCodeFileData.getFile().getAbsolutePath());
+		String relPath = pathHandler
+				.getRelativePathTo(cbmcCodeFileData.getFile());
+		json.put(CBMC_TEST_RUN_CBMC_REL_FILE_PATH_KEY, relPath);
 		json.put(CBMC_GENERATED_CODE_INFO_KEY,
 				CBMCGeneratedCodeInfoSaverLoaderHelper.generatedCodeInfoToJSON(
 						cbmcCodeFileData.getCodeInfo()));
@@ -25,14 +27,18 @@ public class CBMCCodeFileDataSaverLoaderHelper {
 		return json;
 	}
 
-	public static CBMCCodeFileData cbmcCodeFileFromJSON(JSONObject json) {
+	public static CBMCCodeFileData cbmcCodeFileFromJSON(JSONObject json,
+			PathHandler pathHandler) {
 		CBMCCodeFileData cbmcCodeFileData = new CBMCCodeFileData();
 
-		String codeFilePath = json.getString(CBMC_TEST_RUN_CBMC_FILE_PATH_KEY);
+		String relCodeFilePath = json
+				.getString(CBMC_TEST_RUN_CBMC_REL_FILE_PATH_KEY);
+		File codeFile = pathHandler.getFileFromRelativePath(relCodeFilePath);
+		
 		CBMCGeneratedCodeInfo codeInfo = CBMCGeneratedCodeInfoSaverLoaderHelper
 				.generatedCodeInfoFromJSON(
 						json.getJSONObject(CBMC_GENERATED_CODE_INFO_KEY));
-		cbmcCodeFileData.setFile(new File(codeFilePath));
+		cbmcCodeFileData.setFile(codeFile);
 		cbmcCodeFileData.setCodeInfo(codeInfo);
 
 		return cbmcCodeFileData;
